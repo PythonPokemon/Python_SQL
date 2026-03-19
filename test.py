b5a459a24c6a9cde882a867c3cc379effa946d9c
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-03-19 10:35:08 +0100

    neustes Update

---
+++ test.py
@@ -0,0 +1,12 @@
+ganzzahll = 42  # int – ganze Zahl
+print(ganzzahll)  # 42
+
+print(type(ganzzahll))  # <class 'int'>
+
+kommazahl = 3.14  # float – Kommazahl
+print(type(kommazahl))  # <class 'float'>
+
+# Typumwandlung (Casting)
+
+ganzzahl = 10
+print(float(ganzzahl))  # 10.0
